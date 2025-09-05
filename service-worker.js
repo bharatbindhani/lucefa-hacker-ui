18040728e5ab02c6554fba93685e46125681f674
bharatbindhani <bharatbindhanisss363@gmail.com>
2025-09-05 13:25:53 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -18,3 +18,14 @@ function loadName() {
 
   document.getElementById("footer").innerText = "Made for " + savedName + " • by Badal_6174";
 }
+[...savedName].forEach((ch, i) => {
+  const span = document.createElement("span");
+  span.textContent = ch;
+  span.style.animationDelay = (i * 0.2) + "s";
+
+  // Neon green glow effect only
+  span.style.color = "#00ff00";
+  span.style.textShadow = "0 0 5px #00ff00, 0 0 10px #00ff33, 0 0 20px #00ff66";
+
+  logo.appendChild(span);
+});
